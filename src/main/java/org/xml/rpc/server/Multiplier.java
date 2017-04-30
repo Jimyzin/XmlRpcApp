84e50ba0cd447c6d15a2226868d3d0578ea70636
Jimy <Jimy>
2017-04-30 20:37:11 +0800

    Rpc call complete

---
+++ src/main/java/org/xml/rpc/server/Multiplier.java
@@ -0,0 +1,12 @@
+package org.xml.rpc.server;
+
+/**
+ * Created by User on 30-04-2017.
+ */
+public class Multiplier {
+
+    public int multiply(int a, int b){
+        System.out.println("Multiplying: "+a+" x "+b);
+        return (a*b);
+    }
+}
